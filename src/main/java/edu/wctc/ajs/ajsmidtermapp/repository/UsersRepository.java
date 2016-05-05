b193edcec8121d89ef3ccd53dbbf518394df93f8
alygator3017 <aschmidt36@my.wctc.edu>
2016-05-04 20:31:04 -0500

    changed class name Users to User, added repositories and services

--- src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/UsersRepository.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/UsersRepository.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.wctc.ajs.ajsmidtermapp.repository;
+
+import edu.wctc.ajs.ajsmidtermapp.entity.User;
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author Alyson
  */
-public class UsersRepository {
+public interface UsersRepository extends JpaRepository<User, Integer>, Serializable{
     
 }
