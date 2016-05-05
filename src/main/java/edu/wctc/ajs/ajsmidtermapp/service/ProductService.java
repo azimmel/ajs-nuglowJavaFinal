6e709cad812ecb748b022d15b4c13676072d4584
alygator3017 <aschmidt36@my.wctc.edu>
2016-05-04 20:36:39 -0500

    changed class name Users to User, added repositories and services

--- src/main/java/edu/wctc/ajs/ajsmidtermapp/service/ProductService.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/service/ProductService.java
@@ -1,9 +1,56 @@
 package edu.wctc.ajs.ajsmidtermapp.service;
 
+import edu.wctc.ajs.ajsmidtermapp.entity.Product;
+import edu.wctc.ajs.ajsmidtermapp.repository.ProductRepository;
+import java.util.List;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author Alyson
  */
+@Repository
+@Transactional(readOnly = true)
 public class ProductService {
+   private transient final Logger LOG = LoggerFactory.getLogger(ProductService.class);
+
+    @Inject
+    private ProductRepository productRepo;
+
+    public ProductService() {
+    }
+
+    public List<Product> findAll() {
+        return productRepo.findAll();
+    }
     
+
+    public Product findById(String id) {
+        return productRepo.findOne(new Integer(id));
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param product 
+     */
+    @Transactional
+    public void remove(Product product) {
+        LOG.debug("Deleting author: " + product.getProductName());
+        productRepo.delete(product);
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param product 
+     */
+    @Transactional
+    public Product edit(Product product) {
+        return productRepo.saveAndFlush(product);
+    }
 }
--- src/main/java/edu/wctc/ajs/ajsmidtermapp/service/ShoppingCartService.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/service/ShoppingCartService.java
@@ -1,9 +1,57 @@
 package edu.wctc.ajs.ajsmidtermapp.service;
 
+import edu.wctc.ajs.ajsmidtermapp.entity.ShoppingCart;
+import edu.wctc.ajs.ajsmidtermapp.repository.ShoppingCartRepository;
+import java.util.List;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author Alyson
  */
+@Repository
+@Transactional(readOnly = true)
 public class ShoppingCartService {
+       private transient final Logger LOG = LoggerFactory.getLogger(ProductService.class);
+
+    @Inject
+    private ShoppingCartRepository shopRepo;
+
+    public ShoppingCartService() {
+    }
+
+    public List<ShoppingCart> findAll() {
+        return shopRepo.findAll();
+    }
     
+
+    public ShoppingCart findById(String id) {
+        return shopRepo.findOne(new Integer(id));
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param item 
+     */
+    @Transactional
+    public void remove(ShoppingCart item) {
+        LOG.debug("Deleting author: " + item.getProductId().getProductName());
+        shopRepo.delete(item);
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param item 
+     * @return  
+     */
+    @Transactional
+    public ShoppingCart edit(ShoppingCart item) {
+        return shopRepo.saveAndFlush(item);
+    }
 }
