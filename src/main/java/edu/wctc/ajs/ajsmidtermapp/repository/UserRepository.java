57a873bb59b77a560519611692edd9f3de73efbf
alygator3017 <aschmidt36@my.wctc.edu>
2016-05-04 20:40:30 -0500

    changed class name Users to User, added repositories and services

--- src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/UserRepository.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/UserRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  *
  * @author Alyson
  */
-public interface UsersRepository extends JpaRepository<User, Integer>, Serializable{
+public interface UserRepository extends JpaRepository<User, Integer>, Serializable{
     
 }
--- src/main/java/edu/wctc/ajs/ajsmidtermapp/service/UserService.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/service/UserService.java
@@ -5,10 +5,61 @@
  */
 package edu.wctc.ajs.ajsmidtermapp.service;
 
+import edu.wctc.ajs.ajsmidtermapp.entity.ShoppingCart;
+import edu.wctc.ajs.ajsmidtermapp.entity.User;
+import edu.wctc.ajs.ajsmidtermapp.repository.ShoppingCartRepository;
+import edu.wctc.ajs.ajsmidtermapp.repository.UserRepository;
+import java.util.List;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author Alyson
  */
+@Repository
+@Transactional(readOnly = true)
 public class UserService {
+    private transient final Logger LOG = LoggerFactory.getLogger(ProductService.class);
+
+    @Inject
+    private UserRepository userRepo;
+
+    public UserService() {
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
     
+
+    public User findById(String id) {
+        return userRepo.findOne(new Integer(id));
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param user 
+     */
+    @Transactional
+    public void remove(User user) {
+        LOG.debug("Deleting author: " + user.getUsername());
+        userRepo.delete(user);
+    }
+
+    /**
+     * Spring performs a transaction with readonly=false. This
+     * guarantees a rollback if something goes wrong.
+     * @param user
+     * @param item 
+     * @return  
+     */
+    @Transactional
+    public User edit(User user) {
+        return userRepo.saveAndFlush(user);
+    }
 }
