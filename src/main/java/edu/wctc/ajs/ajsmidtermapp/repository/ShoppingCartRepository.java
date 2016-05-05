f16af96752f8628ac5a7018a3fbe41975a5a6c17
alygator3017 <aschmidt36@my.wctc.edu>
2016-05-04 20:30:23 -0500

    changed class name Users to User, added repositories and services

--- src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/ShoppingCartRepository.java
+++ src/main/java/edu/wctc/ajs/ajsmidtermapp/repository/ShoppingCartRepository.java
@@ -5,10 +5,14 @@
  */
 package edu.wctc.ajs.ajsmidtermapp.repository;
 
+import edu.wctc.ajs.ajsmidtermapp.entity.ShoppingCart;
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  *
  * @author Alyson
  */
-public class ShoppingCartRepository {
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer>, Serializable {
     
 }
